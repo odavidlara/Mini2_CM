f7797cfcdc469915ae5b7c54356408ba51d2af43
David Lara <oscar.lr97@gmail.com>
2019-04-18 11:43:36 -0500

    added basic activities for selecting roles

--- app/src/main/java/com/itson/casablancas/mini2_cm/MainActivity.java
+++ app/src/main/java/com/itson/casablancas/mini2_cm/MainActivity.java
@@ -1,13 +1,38 @@
 package com.itson.casablancas.mini2_cm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    Button emisor, receptor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emisor = (Button) findViewById(R.id.btnEmisor);
+        receptor = (Button) findViewById(R.id.btnReceptor);
+
+        emisor.setOnClickListener(this);
+        receptor.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.btnEmisor:
+                startActivity(new Intent(this, Emisor.class));
+                finish();
+                break;
+            case R.id.btnReceptor:
+                startActivity(new Intent(this, Emisor.class));
+                finish();
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/itson/casablancas/mini2_cm/Receptor.java
@@ -0,0 +1,13 @@
+package com.itson.casablancas.mini2_cm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Receptor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receptor);
+    }
+}
